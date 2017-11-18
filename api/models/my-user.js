03442cf35e5aecb74217645116077776d08d21f7
Brian McGee <b.m.mcgee@gmail.com>
2017-11-18 11:25:56 +0100

    Added facebook registration

--- api/models/my-user.js
+++ api/models/my-user.js
@@ -1,82 +1,47 @@
 'use strict';
 
+const graph = require('fbgraph');
+
 module.exports = function(MyUser) {
 
-  MyUser.ROLES = {
-    ADMIN: 'admin',
-    CONTROLLER: 'controller'
-  };
-
-  MyUser.observe('after save', updateRoleMappings(MyUser));
-  MyUser.observe('after delete', cleanupRoleMappings(MyUser));
-
-  MyUser.rolesForUser = rolesForUser(MyUser);
+  MyUser.authenticate = authenticate(MyUser);
 
 };
 
-function rolesForUser(PechUser){
 
-  return function(userId) {
+function authenticate(MyUser) {
 
-    var RoleMapping = PechUser.app.models.RoleMapping;
+  return async (facebookToken, options) => {
 
-    return RoleMapping.find({
-      where: {principalType: RoleMapping.USER, principalId: userId},
-      include: ['role']
-    }).then(function (roleMappings) {
-      return roleMappings.map(function (rm) { return rm.toJSON().role.name; });
+    const profile = await getProfile(facebookToken);
+    const AccessToken = MyUser.app.models.AccessToken;
+
+    const data = {
+      email: profile.email,
+      fbId: profile.id,
+      password: 'pa55word',
+      firstName: profile.first_name,
+      familyName: profile.last_name,
+      location: profile.location ? profile.location.name : null
+    };
+
+    const result = await MyUser.findOrCreate({ where: { fbId: data.fbId }}, data, options);
+    const user = result[0];
+
+    const accessToken = await user.createAccessToken();
+
+    return AccessToken.find({ where: { id: accessToken.id }, include: 'user'})
+  }
+
+}
+
+function getProfile(accessToken) {
+
+  return new Promise((resolve, reject) => {
+    graph.get(`/me?access_token=${accessToken}&fields=id,first_name,last_name,email,location,timezone`, (err, result) => {
+      if(err) return reject(err);
+      resolve(result);
     });
-
-  };
-
-}
-
-function updateRoleMappings(PechUser) {
-
-
-  return function(ctx, next) {
-    const RoleMapping = PechUser.app.models.RoleMapping;
-    const model = ctx.instance;
-    const roles = ctx.options.req.body.roles;
-
-    if (!roles) return next();
-
-    RoleMapping
-      .destroyAll({ principalType: RoleMapping.USER, principalId: model.id })
-      .then(function(){
-        const roleMappings = roles.map(function(role){
-          return { roleId: role, principalType: RoleMapping.USER, principalId: model.id }
-        });
-        return RoleMapping.create(roleMappings);
-      })
-      .then(function(){
-        next();
-      })
-      .catch(function(err){
-        next(err);
-      });
-  }
-
-}
-
-function cleanupRoleMappings(PechUser) {
-
-  return function(ctx, next) {
-
-    if(!(ctx.where || ctx.where.id)) return next();
-
-    const RoleMapping = PechUser.app.models.RoleMapping;
-
-    RoleMapping
-      .destroyAll({ principalType: RoleMapping.USER, principalId: ctx.where.id })
-      .then(function(){
-        next();
-      })
-      .catch(function(err){
-        next(err);
-      });
-
-  }
-
+  });
 
 }
