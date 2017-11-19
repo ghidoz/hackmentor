6c1179251f4dfddad14d71ba7a3c7b70ab4135e9
Brian McGee <b.m.mcgee@gmail.com>
2017-11-19 11:00:38 +0100

    Cleaned up how contact requests are created

--- api/models/contact-request.js
+++ api/models/contact-request.js
@@ -4,5 +4,41 @@ module.exports = (ContactRequest) => {
 
   ContactRequest.validatesInclusionOf('status', { in: ['open', 'accepted', 'declined']});
 
+  ContactRequest.prototype.accept = accept(ContactRequest);
 
+  ContactRequest.observe('before save', setSenderId(ContactRequest));
 };
+
+function setSenderId(ContactRequest) {
+
+  return async (ctx) => {
+
+    console.log('set sender id called', ctx.options.accessToken, ctx.model);
+
+    const model = ctx.instance;
+    if(!model) return null;   // we only handle single model creates
+
+    model.senderId = ctx.options.accessToken.userId;
+  }
+
+}
+
+function accept(ContactRequest) {
+
+  return async (options) => {
+
+    // update the goal
+    const goal = await this.goal(options);
+
+    goal.status = 'assigned';
+    goal.mentorId = this.recipientId;
+
+    await goal.save(options);
+
+    // update this request
+    this.status = 'accepted';
+
+    return this.save(options);
+  }
+
+}
