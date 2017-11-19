6ef70a7a0ba9a46c9a19c6ce71b9d9a643f0f9cb
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2017-11-19 15:09:22 +0100

    Add new goal process

--- client/src/app/goals/goals.module.ts
+++ client/src/app/goals/goals.module.ts
@@ -4,17 +4,23 @@ import { goalsRoutedComponents, GoalsRoutingModule } from './goals.routes';
 import { GoalItemComponent } from './goal-item/goal-item.component';
 import { LayoutModule } from '../layout/layout.module';
 import { MatIconModule } from '@angular/material';
+import { NgbButtonsModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { NgbButtonLabel } from '@ng-bootstrap/ng-bootstrap/buttons/label';
 
 @NgModule({
   imports: [
     CommonModule,
     GoalsRoutingModule,
     LayoutModule,
-    MatIconModule
+    MatIconModule,
+    NgbButtonsModule,
+    FormsModule
   ],
   declarations: [
     goalsRoutedComponents,
     GoalItemComponent
-  ]
+  ],
+  providers: [NgbButtonLabel]
 })
 export class GoalsModule { }
--- client/src/app/goals/new-goal/new-goal.component.ts
+++ client/src/app/goals/new-goal/new-goal.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { HeroCategoryApi } from '../../shared/sdk/services/custom/HeroCategory';
+import { HeroCategory } from '../../shared/sdk/models/HeroCategory';
+import { GoalApi } from '../../shared/sdk/services/custom/Goal';
 
 @Component({
   selector: 'hm-new-goal',
@@ -8,9 +11,65 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class NewGoalComponent implements OnInit {
 
-  constructor() { }
+  public step = 1;
+  public goal: any = {
+    skills: []
+  };
+  public heroCategories: HeroCategory[];
+  public selectedHeroCategory: HeroCategory;
+  public hoursPerWeek: any = [{
+    label: '1-3 Hours',
+    value: 1
+  }, {
+    label: '3-6 Hours',
+    value: 3
+  }, {
+    label: '6-10 Hours',
+    value: 6
+  }, {
+    label: '10+ Hours',
+    value: 10
+  }];
+  public weeklyBudgets: any = [{
+    label: 'Up to 50€',
+    value: 50
+  }, {
+    label: 'Up to 100€',
+    value: 100
+  }, {
+    label: 'Up to 150€',
+    value: 150
+  }, {
+    label: 'Up to 200€',
+    value: 200
+  }];
+
+  constructor(private heroCategoryApi: HeroCategoryApi,
+              private goalApi: GoalApi) {
+  }
 
   ngOnInit() {
+    this.heroCategoryApi.find().subscribe((heroCategories: HeroCategory[]) => {
+      this.heroCategories = heroCategories;
+    });
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  toggleSkill(skill: any) {
+    if (skill.selected) {
+      this.goal.skills.push(skill.id);
+    } else {
+      this.goal.skills = this.goal.skills.filter((skillId) => skillId !== skill.id);
+    }
+  }
+
+  create() {
+    this.goalApi.create(this.goal).subscribe(() => {
+      
+    });
   }
 
 }
