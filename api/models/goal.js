55cb3c1fc1bfedde1b0df9cc5f75b0d974ffd97d
Brian McGee <b.m.mcgee@gmail.com>
2017-11-19 13:37:35 +0100

    Implemented updating of goal skills based on embedded skills list in the request

--- api/models/goal.js
+++ api/models/goal.js
@@ -1,9 +1,13 @@
 
+
+
 module.exports = (Goal) => {
 
   Goal.validatesInclusionOf('status', { in: ['open', 'assigned', 'completed', 'archived']});
 
   Goal.myGoals = myGoals(Goal);
+
+  Goal.observe('after save', updateGoalSkills(Goal));
 };
 
 function myGoals(Goal) {
@@ -13,6 +17,30 @@ function myGoals(Goal) {
     const userId = options.accessToken.userId;
 
     return Goal.find({ where: { apprenticeId: userId }});
+  }
+
+}
+
+function updateGoalSkills(Goal) {
+
+  return async (ctx) => {
+
+    const model = ctx.instance;
+    const skills = ctx.options.req.body.skills;
+
+    if(!(model && skills && Array.isArray(skills))) return;    // we only handle single model updates
+
+    // delete all current goal skills
+    await model.goalSkills.destroyAll();
+
+    // add the new updated list
+    const goalSkills = skills.map((s) => {
+      if(Number.isInteger(s)) return { skillId: s };
+      else if(Object.isPlainObject(s)) return { skillId: s.skillId };
+    });
+
+    await model.goalSkills.create(goalSkills);
+
   }
 
 }
