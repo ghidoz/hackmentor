743ad0e63db5493fc9529ddc52f11c493639087c
Daniele Ghidoli <daniele.ghidoli@bigthink.it>
2017-11-19 12:56:51 +0100

    Add goal module

---
+++ client/src/app/goals/new-goal/new-goal.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'hm-new-goal',
+  templateUrl: './new-goal.component.html',
+  styleUrls: ['./new-goal.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class NewGoalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
